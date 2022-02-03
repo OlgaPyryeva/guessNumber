ce747f5da7b3095730d88be789c378367496c06b
Olga Pyryeva <vashkevicholga@gmail.com>
2022-02-04 00:04:27 +0300

    add(script.js): add adition homework lesson06

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
 "use strict";
 
-//"Загадывание случайного числа от 1 до 100"
-const guessNumber = function (num) {
-  let userNumber = 8;
+let tryGuess = 10;
+let userNumber;
 
-  num = +prompt("Угадай число от 1 до 100");
-
-  if (num === null || num === 0) {
-    alert("Игра окончена");
-  }
-  if (!isNumber(num)) {
-    alert("Введи число!");
-    guessNumber();
-  }
-  if (num > userNumber && num > 0) {
-    alert("Загаданное число меньше");
-    guessNumber();
-  }
-  if (num < userNumber && num > 0) {
-    alert("Загаданное число больше");
-    guessNumber();
-  }
-  if (num === userNumber) {
-    alert("Поздравляю, Вы угадали!!!");
-  }
-};
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function isNumber(number) {
   return !isNaN(parseFloat(number)) && isFinite(number);
 }
 
+const guessNumber = function (num) {
+  num = +prompt("Угадай число от 1 до 100");
+  if (tryGuess == 10) {
+    userNumber = getRandomNumber(1, 100);
+  }
+
+  if (num === null || num === 0 || num == "") {
+    alert("Игра окончена");
+  }
+  if (tryGuess == 0) {
+    if (confirm("Попытки закончились, хотите сыграть еще?") == true) {
+      tryGuess = 10;
+      guessNumber();
+    }
+  }
+  if (!isNumber(num) && tryGuess > 0) {
+    tryGuess--;
+    alert(`Введи число! Осталось попыток ${tryGuess}`);
+    guessNumber();
+  }
+  if (num > userNumber && num > 0 && tryGuess > 0) {
+    tryGuess--;
+    alert(`Загаданное число меньше, осталось попыток ${tryGuess}`);
+    guessNumber();
+  }
+  if (num < userNumber && num > 0 && num != "" && num != null && tryGuess > 0) {
+    tryGuess--;
+    alert(`Загаданное число больше, осталось попыток ${tryGuess}`);
+    guessNumber();
+  }
+  if (num === userNumber && tryGuess > 0) {
+    let repeat = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+    if (repeat == true) {
+      tryGuess = 10;
+      guessNumber();
+    }
+  }
+};
+
 guessNumber();
+
+console.log(userNumber);
